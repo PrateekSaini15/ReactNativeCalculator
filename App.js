eb60fd302229aafdc2eb80cbae815436ae3e0ced
Prateek Saini <prateeksaini15@yahoo.com>
2021-01-10 11:58:09 +0530

    Basic layout is done

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <View style={styles.calculation}></View>
+      <View style={styles.result}></View>
+      <View style={styles.buttons}>
+        <View style={styles.numbers}>
+      
+        </View>
+        <View style={styles.operations}>
+
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  calculation: {
+    flex: 2,
+    backgroundColor: 'red',
+  },
+  result:{
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  buttons:{
+    flex: 5,
+    flexDirection: 'row',
+  },
+  numbers:{
+    flex: 3,
+    backgroundColor: 'green',
+  },
+  operations:{
+    flex:1,
+    backgroundColor: 'orange',
+  }
+
 });

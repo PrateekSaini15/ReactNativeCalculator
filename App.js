c52ddae5628cd220c669129a61a4956b3a17db0e
Prateek Saini <prateeksaini15@yahoo.com>
2021-01-10 15:19:02 +0530

    Buttons added to the layout

--- App.js
+++ App.js
@@ -1,18 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+
+
 
 export default function App() {
+
+  
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.calculation}></View>
-      <View style={styles.result}></View>
+    <View style={styles.calculation}>
+      <Text style={styles.calculationText}>{calculationText}</Text>
+    </View>
+      <View style={styles.result}>
+      <Text style={styles.resultText}>{resultText}</Text>
+      </View>
       <View style={styles.buttons}>
         <View style={styles.numbers}>
-      
+          <View style={styles.row}>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>9</Text>
+          </TouchableOpacity>
+          
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>6</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>=</Text>
+          </TouchableOpacity>
+        </View>
         </View>
         <View style={styles.operations}>
-
+        <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>*</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.opacButton}>
+            <Text style={styles.buttonText}>/</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
@@ -26,10 +89,14 @@ const styles = StyleSheet.create({
   calculation: {
     flex: 2,
     backgroundColor: 'red',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
   },
   result:{
     flex: 1,
     backgroundColor: 'yellow',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
   },
   buttons:{
     flex: 5,
@@ -42,6 +109,26 @@ const styles = StyleSheet.create({
   operations:{
     flex:1,
     backgroundColor: 'orange',
+    justifyContent: 'space-around'
+  },
+  row:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  opacButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 30
+  },
+  resultText: {
+    fontSize: 30,
+  },
+  calculationText: {
+    fontSize: 30,
   }
-
 });
